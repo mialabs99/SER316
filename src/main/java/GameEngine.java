980d44f715a028807c27dd81e68fee33fe2d1f0d
Mia Laboranti <mia_laboranti@hotmail.com>
2025-10-18 09:52:17 -0400

    Continued fixing merge conflicts

--- src/main/java/GameEngine.java
+++ src/main/java/GameEngine.java
@@ -11,19 +11,26 @@ public class GameEngine {
         this.max = max;
         this.attempts = 0;
         this.gameWon = false;
+        this.userQuit = false;
         reset();
     }
 
     public GuessResult makeGuess(int guess) {
+        // Check if user wants to quit (negative number)
+        if (guess < 0) {
+            userQuit = true;
+            return new GuessResult(false, "Exiting game...", attempts);
+            }
+
         attempts++;
 
         if (guess == target) {
             gameWon = true;
             return new GuessResult(true, "Correct! You guessed it in " + attempts + " attempts.", attempts);
         } else if (guess < target) {
-            return new GuessResult(false, "Too low!", attempts);
+            return new GuessResult(false, "Too low! Try a higher number.", attempts);
         } else {
-            return new GuessResult(false, "Too high!", attempts);
+            return new GuessResult(false, "Too high! Try a lower number.", attempts);
         }
     }
 
